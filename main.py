3072fd21c89bef7e8733d6eb53efe2fa480c08ff
Vishal <vishalchauhan3549@gmail.com>
2023-11-10 09:34:46 +0000

    Added the idj parameter in encrypT() sunction call

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ keywords = ['medical', 'data', 'covid']
 n = len(keywords)
 
 idi, idt,idk,idl, idj=patient_id,doctor_id,dataConsumer_id,cloud_id,hospital_id
-wi,R_t,V_t,C_wi,C_id,t_i=encrypT(params,EMR, qki, qkl, skt, ski, qkt, idi, idt, del_i, keywords)
+wi,R_t,V_t,C_wi,C_id,t_i=encrypT(params,EMR, qki, qkl, skt, ski, qkt, idi, idt, del_i, keywords, idj)
 print("Cwi",C_wi)
 
 T_w,T_id,t_i1,w_j=trapdooR(params,skk,ski,qki,qkk,idi,idk,del_i,keywords)
